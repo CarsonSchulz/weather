f165d92c4b99bb35e77cab90768f8e955ea25d70
Carson <schulzcarson@gmail.com>
2020-01-14 15:38:13 -0500

    Cosmetics

--- js/geolocation.js
+++ js/geolocation.js
@@ -1,14 +1,13 @@
-$(document).ready(function() {
+$(document).ready(function($) {
     //  Start by grabbing the GPS location
     tryGPS();
-
     //  Check to see if there is any values for the lat and long
     setTimeout(function(){
         var lat = parseFloat(document.getElementById('locationLat').innerHTML);
         var lon = parseFloat(document.getElementById('locationLon').innerHTML);
         //  Uncomment below to set a manual lat/lon
-        lat = 47.6036;
-        lon = -122.3294;
+        lat = 22.325663;
+        lon = 114.198654;
         //  Check to see if they exist, if not fill with a default. Orlando in this case.
         if(lat === "" || lon === ""){
             lat = 28;
@@ -18,7 +17,6 @@ $(document).ready(function() {
         //  Build a URL for the call
         var getURL = 'pulldata.php?lat=' + lat + '&lon=' + lon;
 
-        
         //  Begin the calls
         $.ajax({ 
             type: 'GET', 
@@ -34,7 +32,6 @@ $(document).ready(function() {
                     weatherCity = data.city;
                     weatherState = data.state;
 
-                    //  Display weather data
                     document.getElementById("displayLocation").innerHTML = weatherLoc;
                     document.getElementById("displayTemp").innerHTML = weatherTemp;
                     document.getElementById("locationCity").innerHTML = weatherCity;
@@ -46,18 +43,18 @@ $(document).ready(function() {
 
                     var weatherIconHTML = document.getElementById("weatherIcon");
                     weatherIconHTML.classList.add(weatherIconClass);
+
+                    var backgroundVidSrc = backgroundVid(data.icon, tod);
+                    var video = document.getElementById('headerVideo');
+                    video.src = 'videos/' + backgroundVidSrc;
+                    video.play();
                 }
             }
         });
-        
-
-
-
 
     }, 300);
     
 });
-
 
 function tryGPS() {
     if (navigator.geolocation) {
@@ -93,4 +90,91 @@ function nightOrDay(sunrise, sunset) {
 function weatherIcon(code, tod) {
     var iconClass = 'wi-owm-' + tod + '-' + code; 
     return iconClass;
+}
+function backgroundVid(code, tod) {
+
+    //  Thunderstorm
+    if(code >= 200 && code <= 299) {
+        if(tod === 'day') {
+            return 'thunderstormday.mp4';
+        } else {
+            return 'thunderstormnight.mp4';
+        }
+    }
+    //  Drizzle
+    else if(code >= 300 && code <= 399) {
+        if(tod === 'day') {
+            return 'drizzleday.mp4';
+        } else {
+            return 'drizzlenight.mp4';
+        }
+    }
+    //  Rain
+    else if(code >= 500 && code <= 599) {
+        if(tod === 'day') {
+            return 'rainday.mp4';
+        } else {
+            return 'rainnight.mp4';
+        }
+    }
+    //  Snow
+    else if(code >= 600 && code <= 699) {
+        if(tod === 'day') {
+            return 'snowday.mp4';
+        } else {
+            return 'snownight.mp4';
+        }
+    }
+    //  Atmosphere
+    else if(code == 701) {
+        return 'mist.mp4';
+    }
+    else if(code == 711) {
+        return 'smoke.mp4';
+    }
+    else if(code == 721) {
+        return 'haze.mp4';
+    }
+    else if(code == 731) {
+        return 'dust.mp4';
+    }
+    else if(code == 741) {
+        if(tod === 'day') {
+            return 'fogday.mp4';
+        } else {
+            return 'fognight.mp4';
+        }
+    }
+    else if(code == 751) {
+        return 'sand.mp4';
+    }
+    else if(code == 761) {
+        return 'dust.mp4';
+    }
+    else if(code == 762) {
+        return 'dust.mp4';
+    }
+    else if(code == 771) {
+        return 'squalls.mp4';
+    }
+    else if(code == 781) {
+        return 'tornado.mp4';
+    }
+    else if(code == 800) {
+        if(tod === 'day') {
+            return 'clearday.mp4';
+        } else {
+            return 'clearnight.mp4';
+        }
+    }
+    else if(code > 800) {
+        if(tod === 'day') {
+            return 'cloudyday.mp4';
+        } else {
+            return 'cloudynight.mp4';
+        }
+    }
+    else {
+        return "poop";
+    }
 }
